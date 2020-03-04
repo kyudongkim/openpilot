adf58442c5c3fad0f2b646506e5c2671619bbe6f
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-03-05 08:38:07 +0900

    Update values.py
    CARDENZA(K7) UPDATE

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -3,24 +3,26 @@ from selfdrive.car import dbc_dict
 Ecu = car.CarParams.Ecu
 
 class CAR:
-  GENESIS = "HYUNDAI GENESIS 2018 & 2014"
+  GENESIS = "HYUNDAI GENESIS 2014-2018"
   GENESIS_G80 = "GENESIS G80 2017"
   GENESIS_G90 = "GENESIS G90 2017 & EQ900"  
   ELANTRA = "HYUNDAI ELANTRA LIMITED ULTIMATE 2017"
   ELANTRA_GT_I30 = "HYUNDAI I30 N LINE 2019 & GT 2018 DCT"
   SONATA = "HYUNDAI SONATA 2018"
   GRANDEUR = "HYUNDAI GRANDEUR IG 2017"
-  GRANDEUR_HEV = "HYUNDAI GRANDEUR IG HYBRID 2019"
-  SANTA_FE = "HYUNDAI SANTA FE LIMITED 2019"
-  IONIQ = "HYUNDAI IONIQ HYBRID 2017"
-  IONIQ_EV = "HYUNDAI IONIQ ELECTRIC 2019"
+  GRANDEUR_HEV = "HYUNDAI GRANDEUR IG HEV 2019"
+  SANTA_FE = "HYUNDAI SANTA FE 2019"
+  IONIQ = "HYUNDAI IONIQ HEV 2017"
+  IONIQ_EV = "HYUNDAI IONIQ EV 2019"
   KONA = "HYUNDAI KONA 2019"
-  KONA_EV = "HYUNDAI KONA ELECTRIC 2019"
-  KIA_OPTIMA = "KIA OPTIMA SX 2019 & 2016"
-  KIA_OPTIMA_H = "KIA OPTIMA HYBRID 2017 & SPORTS 2019"
-  KIA_FORTE = "KIA FORTE E 2018"
-  KIA_SORENTO = "KIA SORENTO GT LINE 2018"
-  KIA_STINGER = "KIA STINGER GT2 2018"
+  KONA_EV = "HYUNDAI KONA EV 2019"
+  KIA_OPTIMA = "KIA OPTIMA SX 2016-2019"
+  KIA_OPTIMA_HEV = "KIA OPTIMA HEV 2016-2019"
+  KIA_CARDENZA = "KIA CARDENZA 2016-2019"
+  KIA_CARDENZA_HEV = "KIA CARDENZA HEV 2016-2019"
+  KIA_FORTE = "KIA FORTE 2018"
+  KIA_SORENTO = "KIA SORENTO 2018"
+  KIA_STINGER = "KIA STINGER 2018"
   KIA_SELTOS = "KIA SELTOS 2019"
   KIA_NIRO_EV = "KIA NIRO 2016"
 
@@ -117,11 +119,15 @@ FINGERPRINTS = {
   {
     64: 8, 66: 8, 67: 8, 68: 8, 127: 8, 128: 8, 129: 8, 273: 8, 274: 8, 275: 8, 339: 8, 354: 3, 356: 4, 399: 8, 512: 6, 544: 8, 608: 8, 790: 8, 809: 8, 832: 8, 899: 8, 902: 8, 903: 6, 916: 8, 1040: 8, 1056: 8, 1057: 8, 1078: 4, 1151: 6, 1168: 7, 1170: 8, 1265: 4, 1280: 1, 1282: 4, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1331: 8, 1332: 8, 1333: 8, 1342: 6, 1345: 8, 1348: 8, 1349: 8, 1351: 8, 1353: 8, 1356: 8, 1363: 8, 1365: 8, 1366: 8, 1367: 8, 1369: 8, 1407: 8, 1415: 8, 1419: 8, 1427: 6, 1440: 8, 1456: 4, 1470: 8, 1472: 8, 1491: 8, 1492: 8, 1905: 8, 1913: 8, 2001: 8, 2009: 8, 2015: 8, 2016: 8, 2017: 8, 2024: 8, 2025: 8
   }],
-  CAR.KIA_OPTIMA_H: [{
+  CAR.KIA_OPTIMA_HEV: [{
     68: 8, 127: 8, 304: 8, 320: 8, 339: 8, 352: 8, 356: 4, 544: 8, 593: 8, 688: 5, 832: 8, 881: 8, 882: 8, 897: 8, 902: 8, 903: 6, 916: 8, 1040: 8, 1056: 8, 1057: 8, 1078: 4, 1136: 6, 1151: 6, 1168: 7, 1173: 8, 1236: 2, 1265: 4, 1280: 1, 1287: 4, 1290: 8, 1291: 8, 1292: 8, 1322: 8, 1331: 8, 1332: 8, 1333: 8, 1342: 6, 1345: 8, 1348: 8, 1355: 8, 1363: 8, 1369: 8, 1371: 8, 1407: 8, 1419: 8, 1427: 6, 1429: 8, 1430: 8, 1448: 8, 1456: 4, 1470: 8, 1476: 8, 1535: 8
   },
   {
     68: 8, 127: 8, 304: 8, 320: 8, 339: 8, 352: 8, 356: 4, 544: 8, 576: 8, 593: 8, 688: 5, 881: 8, 882: 8, 897: 8, 902: 8, 903: 8, 909: 8, 912: 7, 916: 8, 1040: 8, 1056: 8, 1057: 8, 1078: 4, 1136: 6, 1151: 6, 1168: 7, 1173: 8, 1180: 8, 1186: 2, 1191: 2, 1265: 4, 1268: 8, 1280: 1, 1287: 4, 1290: 8, 1291: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1342: 6, 1345: 8, 1348: 8, 1355: 8, 1363: 8, 1369: 8, 1371: 8, 1407: 8, 1419: 8, 1420: 8, 1425: 2, 1427: 6, 1429: 8, 1430: 8, 1448: 8, 1456: 4, 1470: 8, 1476: 8, 1535: 8
+  }],
+  CAR.KIA_CARDENZA: [{
+  }],
+  CAR.KIA_CARDENZA_HEV: [{
   }],
   CAR.KIA_FORTE: [{
     67: 8, 127: 8, 304: 8, 320: 8, 339: 8, 356: 4, 544: 8, 593: 8, 608: 8, 688: 5, 809: 8, 832: 8, 854: 7, 870: 7, 871: 8, 872: 8, 897: 8, 902: 8, 903: 8, 909: 8, 916: 8, 1040: 8, 1042: 8, 1078: 4, 1107: 5, 1136: 8, 1156: 8, 1170: 8, 1173: 8, 1191: 2, 1225: 8, 1265: 4, 1280: 4, 1287: 4, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1342: 6, 1345: 8, 1348: 8, 1363: 8, 1369: 8, 1384: 8, 1394: 8, 1407: 8, 1427: 6, 1456: 4, 1470: 8
@@ -148,9 +154,12 @@ CHECKSUM = {
 }
 
 FEATURES = {
-  "use_cluster_gears": [CAR.ELANTRA, CAR.KONA, CAR.ELANTRA_GT_I30],     # Use Cluster for Gear Selection, rather than Transmission
-  "use_tcu_gears": [CAR.KIA_OPTIMA, CAR.SONATA],                                    # Use TCU Message for Gear Selection
-  "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.IONIQ_EV, CAR.KONA_EV, CAR.KIA_NIRO_EV, CAR.GRANDEUR_HEV],              # Use TCU Message for Gear Selection
+ # Use Cluster for Gear Selection, rather than Transmission
+  "use_cluster_gears": [CAR.ELANTRA, CAR.KONA, CAR.ELANTRA_GT_I30],
+ # Use TCU Message for Gear Selection
+  "use_tcu_gears": [CAR.KIA_OPTIMA, CAR.SONATA, CAR.KIA_CARDENZA],
+ # Use elect Message for Gear Selection
+  "use_elect_gears": [CAR.KIA_OPTIMA_HEV, CAR.IONIQ_EV, CAR.KONA_EV, CAR.KIA_NIRO_EV, CAR.GRANDEUR_HEV, CAR.KIA_CARDENZA_HEV],
 }
 
 DBC = {
@@ -168,7 +177,9 @@ DBC = {
   CAR.KONA: dbc_dict('hyundai_kia_generic', None),
   CAR.KONA_EV: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_OPTIMA: dbc_dict('hyundai_kia_generic', None),
-  CAR.KIA_OPTIMA_H: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_OPTIMA_HEV: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_CARDENZA: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_CARDENZA_HEV: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_FORTE: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_SORENTO: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_STINGER: dbc_dict('hyundai_kia_generic', None),
