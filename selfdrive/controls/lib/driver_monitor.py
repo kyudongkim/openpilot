e435bd77688ce385d1a01b6f82e6bb4ec745c0b5
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-03-05 23:11:02 +0900

    Update driver_monitor.py

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -6,8 +6,8 @@ from common.filter_simple import FirstOrderFilter
 from common.stat_live import RunningStatFilter
 
 _AWARENESS_TIME = 3600.  # 60 minute limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 60.  # a first alert is issued 25s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 30.  # a second alert is issued 15s before start decelerating the car
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 60.  # a first alert is issued 60sec before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 30.  # a second alert is issued 30sec before start decelerating the car
 _DISTRACTED_TIME = 60.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 30.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 15.
@@ -37,8 +37,8 @@ _POSE_OFFSET_MAX_COUNT = 360 # stop deweighting new data after 6 min, aka "short
 _RECOVERY_FACTOR_MAX = 5. # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25 # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 10 # not allowed to engage after 3 terminal alerts
-MAX_TERMINAL_DURATION = 1200 # 30s
+MAX_TERMINAL_ALERTS = 10 # not allowed to engage after 10 terminal alerts
+MAX_TERMINAL_DURATION = 1200 # 2 miniute
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
 RESIZED_FOCAL = 320.0
