4a4ac1938675279f9864952fa6967a5de4470610
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-03-13 10:21:35 +0900

    Update ui.cc
    dashcam remove

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -957,7 +957,7 @@ int main(int argc, char* argv[]) {
 
     // Don't waste resources on drawing in case screen is off or car is not started.
     if (s->awake && s->vision_connected) {
-      dashcam(s, touch_x, touch_y);
+    //  dashcam(s, touch_x, touch_y);
       ui_draw(s);
       glFinish();
       should_swap = true;
