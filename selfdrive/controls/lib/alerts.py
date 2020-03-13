692373df1aa05aeafb5697636a3aa9fde24d52ff
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-03-13 20:26:46 +0900

    Update alerts.py

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -63,7 +63,7 @@ ALERTS = [
       "방향지시등 동작시에는 핸들조향 꺼짐",
       "",
       AlertStatus.userPrompt, AlertSize.small,
-      Priority.MID, VisualAlert.none, AudibleAlert.none, 0., 0., .1),
+      Priority.HIGH, VisualAlert.none, AudibleAlert.none, 0., 0., .1),
   Alert(
       "lkasButtonOff",
       "LKAS 버튼 꺼짐",
