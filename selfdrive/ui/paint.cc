14fd3279face27aa170ebe344ea9e100dbe35465
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-03-15 22:50:13 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1029,7 +1029,7 @@ static void ui_draw_vision_event(UIState *s) {
     nvgFill(s->vg); */
     nvgBeginPath(s->vg);
     NVGpaint imgPaint = nvgImagePattern(s->vg, img_wheel_x-bg_wheel_x, img_wheel_y-(bg_wheel_y + (bdr_s*1.5)),
-  img_wheel_size, img_wheel_size, 0, s->img_wheel, img_wheel_alpha);
+                                        img_wheel_size, img_wheel_size, 0, s->img_wheel, img_wheel_alpha);
     nvgRect(s->vg, img_wheel_x-bg_wheel_x, img_wheel_y-(bg_wheel_y + (bdr_s*1.5)), img_wheel_size, img_wheel_size);
     nvgFillPaint(s->vg, imgPaint);
     nvgFill(s->vg);
