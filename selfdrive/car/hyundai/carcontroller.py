785bcc6c74a599bab43a1466b1788c85acfd64da
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-10-19 20:12:51 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -123,12 +123,12 @@ class CarController():
       lkas_active = False
 
     # Disable steering while turning blinker on and speed below 60 kph
-    elif CS.out.leftBlinker or CS.out.rightBlinker:
-      self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
+#    elif CS.out.leftBlinker or CS.out.rightBlinker:
+#      self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
 #    if self.turning_indicator_alert: # set and clear by interface
-      lkas_active = 0
-    if self.turning_signal_timer > 0:
-      self.turning_signal_timer -= 1
+#      lkas_active = 0
+#    if self.turning_signal_timer > 0:
+#      self.turning_signal_timer -= 1
 
     if not lkas_active:
       apply_steer = 0
