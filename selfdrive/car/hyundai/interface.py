88493bbbc0bc62c5f439de5e273ca49f55dfd2f0
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-10-26 08:15:53 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -34,10 +34,10 @@ class CarInterface(CarInterfaceBase):
     # Most Hyundai car ports are community features for now
     ret.communityFeature = candidate not in [CAR.SONATA, CAR.PALISADE]
 
-    ret.steerActuatorDelay = 0.1  # Default delay
-    ret.steerRateCost = 0.5
-    ret.steerLimitTimer = 0.8
-    tire_stiffness_factor = 1.
+    ret.steerActuatorDelay = 0.1      # 0.1
+    ret.steerRateCost = 0.25          # 0.5
+    ret.steerLimitTimer = 1.2         # 0.8
+    tire_stiffness_factor = 1.        # 1 / 0.5 / 0.385
 
     # genesis
     if candidate == CAR.GENESIS:
