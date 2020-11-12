d0a912e7bf3bbd7ba917464884a7b8558f255fec
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-11-12 18:30:42 +0900

    fix

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -35,12 +35,15 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     # Note: the warning is hidden while the blinkers are on
     values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
 
-    # This field is actually LdwsActivemode ( only use ldws camera )    
+    # This field is actually LdwsActivemode ( only use ldws camera )
   elif car_fingerprint == CAR.GENESIS:
-    values["CF_Lkas_LdwsActivemode"] = 2    
+    values["CF_Lkas_LdwsActivemode"] = 2
   elif car_fingerprint in [CAR.OPTIMA, CAR.OPTIMA_HEV, CAR.CADENZA, CAR.CADENZA_HEV]:
     values["CF_Lkas_LdwsActivemode"] = 0
-
+#    values["CF_Lkas_LdwsOpt_USM"] = 3
+#    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
+#    values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
+    
   dat = packer.make_can_msg("LKAS11", 0, values)[2]
 
   if car_fingerprint in CHECKSUM["crc8"]:
